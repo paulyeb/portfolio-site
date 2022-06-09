ec8584d40f0e8c656cbc5b8a258a96cd68157207
Paul Yeboah Asamoah <yeboaasamoah@gmail.com>
2022-06-09 18:40:43 +0000

    make it better

--- src/components/Modals/ErrorModal.js
+++ src/components/Modals/ErrorModal.js
@@ -2,8 +2,8 @@ import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ErrorModal = ({dismiss}) => {
-    return <div onClick={dismiss} className="flex items-center fixed inset-0 bg-gray-200 bg-opacity-30 z-10 w-auto h-auto">
-        <div className="flex justify-between items-center container w-auto text-center text-xl font-medium shadow-lg h-20 bg-white border">
+    return <div onClick={dismiss} className="flex items-center fixed inset-0 bg-gray-400 bg-opacity-30 z-10 w-auto h-auto">
+        <div className="flex justify-between items-center container p-2 lg:mx-auto mx-10 w-auto text-center text-xl font-medium shadow-lg h-20 bg-white border">
             <FontAwesomeIcon icon={faCircleXmark} style={{height: '20px', width: '20px', color: 'red' }} className="mx-2" /> 
             Oops! Sorry, something went wrong, please try again.
         </div>
--- src/components/Modals/SuccessModal.js
+++ src/components/Modals/SuccessModal.js
@@ -2,8 +2,8 @@ import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SuccessModal = ({dismiss}) => {
-    return <div onClick={dismiss} className="flex items-center fixed inset-0 bg-gray-200 bg-opacity-30 z-10 w-auto h-auto">
-        <div className="flex justify-between items-center container w-auto text-center text-xl font-medium shadow-lg h-20 bg-white border">
+    return <div onClick={dismiss} className="flex items-center fixed inset-0 bg-gray-400 bg-opacity-30 z-10 w-auto h-auto">
+        <div className="flex justify-between items-center container p-2 lg:mx-auto mx-10 w-auto text-center text-xl font-medium shadow-lg h-20 bg-white border">
             <FontAwesomeIcon icon={faCircleCheck} style={{height: '20px', width: '20px', color: 'green' }} className="mx-2" /> 
             Fantastic! Your message was successfuly sent.
         </div>
