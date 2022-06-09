bbd1a89d85ec1de5928f3ce95e159a2c5bf74be7
Paul Yeboah Asamoah <yeboaasamoah@gmail.com>
2022-06-09 18:48:31 +0000

    make it better

--- src/components/Modals/SuccessModal.js
+++ src/components/Modals/SuccessModal.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SuccessModal = ({dismiss}) => {
     return <div onClick={dismiss} className="flex items-center fixed inset-0 bg-gray-400 bg-opacity-30 z-10 w-auto h-auto">
-        <div className="flex justify-between items-center container p-2 lg:mx-auto mx-10 w-auto text-center text-xl font-medium shadow-lg h-20 bg-white border">
+        <div className="flex justify-between items-center container rounded-xl p-2 sm:mx-auto mx-6 w-auto text-center text-xl font-medium shadow-lg h-20 bg-white border">
             <FontAwesomeIcon icon={faCircleCheck} style={{height: '20px', width: '20px', color: 'green' }} className="mx-2" /> 
             Fantastic! Your message was successfuly sent.
         </div>
